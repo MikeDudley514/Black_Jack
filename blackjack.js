0d958e4e7ed231a22f7a6b18848e62ed73d6aae2
Your Name <you@example.com>
2025-04-21 12:30:58 -0400

    deck creation

---
+++ blackjack.js
@@ -0,0 +1,42 @@
+var dealerSum = 0;
+var yourSum = 0;
+
+var dealerAceCount = 0;
+var yourAceCount = 0;
+
+var hidden;
+var deck;
+
+var canHit = true;
+
+window.onload = function () {
+  buildDeck();
+  shuffleDeck();
+};
+
+function buildDeck() {
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let types = ["C", "D", "H", "S"];
+  deck = [];
+
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      deck.push(values[j] + "-" + types[i]);
+    }
+  }
+  console.log(deck);
+}
